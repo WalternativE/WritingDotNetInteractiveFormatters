6fcbe3e6775a82e803a1995eb2f10eaebcfeb1d2
WalternativE <gregor.beyerle@gmail.com>
2020-11-09 16:03:30 +0100

    Initial commit

---
+++ Dotnet.Interactive.Extension.Plotly.Net/Formatter.fs
@@ -0,0 +1,34 @@
+﻿namespace Dotnet.Interactive.Extension.Plotly.Net
+
+open System
+open System.IO
+open System.Threading.Tasks
+open Microsoft.DotNet.Interactive
+open Microsoft.DotNet.Interactive.Formatting
+open Plotly.NET
+open Plotly.NET.GenericChart
+
+type PlotlyNetFormatterKernelExtension() =
+
+    let registerFormatter () =
+        Formatter.Register<GenericChart.GenericChart>
+            (Func<FormatContext, GenericChart.GenericChart, TextWriter, bool>(fun context chart writer ->
+                let html = toChartHTML chart
+                writer.Write(html)
+
+                true),
+             HtmlFormatter.MimeType)
+
+    interface IKernelExtension with
+        member _.OnLoadAsync _ =
+            registerFormatter ()
+
+            if isNull KernelInvocationContext.Current |> not then
+                let message =
+                    (nameof PlotlyNetFormatterKernelExtension, nameof GenericChart)
+                    ||> sprintf "Added %s including formatters for %s"
+
+                KernelInvocationContext.Current.Display(message, "text/markdown")
+                |> ignore
+
+            Task.CompletedTask
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Writing Dotnet Interactive Formatters
+
+Ever wanted to know how you can extend dotnet interactive with custom formatters? Yes? Good!
+In this repo you can find [a notebook](Formatters.ipynb), that takes a look at how some custom
+.NET/F# types get formatted and how you can add little extras for your use cases. No in-depth look
+but enough to get you started quickly.
+
+If you want to go a bit further and share your formatters (and other extensions) with others
+it would make sense to ship them as reusable NuGet packages. This repo contains a project
+as well as an [accompanying notebook](ExtensionDevelopment.ipynb), that should point you in the right
+direction for this task.
